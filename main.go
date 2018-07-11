7814c09fa0977ea187bc4df9a6b22287aabca08e
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-07-12 00:38:45 +0600

    saveToFile

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
-
-	/*
-		hand, remainingCards := deal(cards, 5)
-		hand.print()
-		remainingCards.print() */
+	cards.saveToFile("data_cards")
 
 }
