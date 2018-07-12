2777dd930eda269f99d30a85d3d756977ab0cb78
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-07-12 17:00:13 +0600

    shuffle

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck' which is a slice of strings
@@ -14,7 +16,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"♠", "♦", "♥", "♣"}
-	cardVlues := []string{"Туз", "2", "3", "4"}
+	cardVlues := []string{"T", "2", "3", "4"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardVlues {
@@ -52,4 +54,14 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("data_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 
 }
