b40eeeae928d34dc2c135127d855b1025d72c41a
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-07-13 16:20:08 +0600

    Update TestNewDeck

--- deck_test.go
+++ deck_test.go
@@ -4,7 +4,16 @@ import "testing"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
+
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "T♠" {
+		t.Errorf("Expected first card of T♠, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "4♣" {
+		t.Errorf("Expected first card of 4♣, but got %v", d[0])
 	}
 }
